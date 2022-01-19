606c82603502b4f3d2d42161f69ca90e40b52eb3
Milli Guleria <milliguleria@pc-134-147.customer.ask4.lan>
2022-01-19 17:54:44 +0000

    initial commit

---
+++ solution.py
@@ -0,0 +1,14 @@
+import json
+from unittest import result
+
+
+def count_clicks(domain_dict):
+    result = {}
+
+    return result
+
+
+if __name__ == '__main__':
+    domain_dict = '{"amazon.com":23,"google.co.uk":15,"google.com":7,"amazon.co.uk":9,"london.gov.uk":4}'
+    result = count_clicks(domain_dict)
+    print(result)
