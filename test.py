6fb2c5ebb346a4c97ab6e141d0584a33a9b5fc18
Milli Guleria <milliguleria@pc-134-147.customer.ask4.lan>
2022-01-19 18:12:03 +0000

    Added some test cases(TDD)

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 import unittest
+import solution
 
 class TestStringMethods(unittest.TestCase):
 
+    def test_base_case(self):
+        self.assertEqual(solution.count_clicks('{"amazon.com":23,"google.co.uk":15,"google.com":7,"amazon.co.uk":9,"london.gov.uk":4}'), {'amazon.com': 23, 'com': 30, 'google.co.uk': 15, 'uk': 28, 'co.uk': 24, 'google.com': 7, 'amazon.co.uk': 9, 'london.gov.uk': 4, 'gov.uk': 4}
+                         )
+
+    def test_case1(self):
+        self.assertEqual(solution.count_clicks('{"google.co.uk":15}'), {
+                         'google.co.uk': 15, 'uk': 15, 'co.uk': 15})
+
+    def test_case2(self):
+        self.assertEqual(solution.count_clicks('{"amazon.com":23,"google.co.uk":15}'), {
+                         'amazon.com': 23, 'com': 23, 'google.co.uk': 15, 'uk': 15, 'co.uk': 15})
 
     
 if __name__ == '__main__':
